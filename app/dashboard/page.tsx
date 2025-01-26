eed5f32bc5f1ac3924b04248071c4f6056a3ea6e
sathindudezoysa <sathindu.d.zoysa@gmail.com>
2025-01-26 17:48:00 +0530

    Revert "Adding Dashboard UI"
    This reverts commit 3dc2819f19c91862fb1d702fa0963d0ee9d65d6d.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,6 @@
 import { useEffect } from "react";
 import { redirect } from "next/navigation";
 import { retrieveUserState } from "../lib/seesion_coockie";
-import { KeyIcon } from "@heroicons/react/24/outline";
 
 export default function Dashboard() {
   // useEffect(() => {
@@ -17,97 +16,7 @@ export default function Dashboard() {
 
   return (
     <>
-      <div className="w-full">
-        <div className="m-10 text-3xl font-bold">Dashboard</div>
-        <div className="relative overflow-x-auto sm:rounded-lg m-10">
-          <table className="w-full text-sm text-left rtl:text-right">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
-              <tr>
-                <th scope="col" className="px-6 py-3"></th>
-                <th scope="col" className="px-6 py-3">
-                  Name
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Email
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Key ID
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Created
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  <span className="sr-only">Edit</span>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="bg-white border-b border-gray-200 hover:bg-gray-50">
-                <td>
-                  <KeyIcon className="size-6" />
-                </td>
-                <td
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
-                >
-                  Apple MacBook Pro 17"
-                </td>
-                <td className="px-6 py-4">Silver</td>
-                <td className="px-6 py-4">Laptop</td>
-                <td className="px-6 py-4">$2999</td>
-                <td className="px-6 py-4 text-right">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 hover:underline"
-                  >
-                    Edit
-                  </a>
-                </td>
-              </tr>
-              <tr className="bg-white border-b border-gray-200 hover:bg-gray-50">
-                <td></td>
-                <td
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
-                >
-                  Microsoft Surface Pro
-                </td>
-                <td className="px-6 py-4">White</td>
-                <td className="px-6 py-4">Laptop PC</td>
-                <td className="px-6 py-4">$1999</td>
-                <td className="px-6 py-4 text-right">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 hover:underline"
-                  >
-                    Edit
-                  </a>
-                </td>
-              </tr>
-              <tr className="bg-white hover:bg-gray-50">
-                <td></td>
-                <td
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
-                >
-                  Magic Mouse 2
-                </td>
-                <td className="px-6 py-4">Black</td>
-                <td className="px-6 py-4">Accessories</td>
-                <td className="px-6 py-4">$99</td>
-                <td className="px-6 py-4 text-right">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 hover:underline"
-                  >
-                    Edit
-                  </a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <div></div>
     </>
   );
 }
